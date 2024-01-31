fb265356d14ca316e5f540a364ece9c3053a1571
jordanbrown0606 <157506730+jordanbrown0606@users.noreply.github.com>
2024-01-31 09:53:33 -0600

    Added scripts for moving platforms

--- Assets/Scripts/Moving Platform.cs
+++ Assets/Scripts/Moving Platform.cs
@@ -21,12 +21,40 @@ public class MovingPlatform : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        targetNextWaypoint();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        elapsedTime = Time.deltaTime;
+
+        float elapsedPercentage = elapsedTime / timeToWaypoint;
+        transform.position = Vector3.Lerp(previousWaypoint.position, targetWaypoint.position, elapsedPercentage);
+
+        if (elapsedPercentage >= 1)
+        {
+            targetNextWaypoint();
+        }
+    }
+
+    private void targetNextWaypoint()
+    { 
+        previousWaypoint = waypointPath.GetWaypoint(targetWaypointIndex);
+        targetWaypointIndex = waypointPath.GetNextWaypoint(targetWaypointIndex);
+        targetWaypoint = waypointPath.GetWaypoint(targetWaypointIndex);
+
+        elapsedTime = 0;
+
+        float distanceToWaypoint = Vector3.Distance(previousWaypoint.position, targetWaypoint.position);
+        timeToWaypoint = distanceToWaypoint / speed;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player");
+        { 
+            other.transform.SetParent(transform);
+        }
     }
 }
